25a9bada3ee4e1368571cf22d4f30518b0affb66
Jan Petter <janpette@elkjop.no>
2023-11-12 14:33:33 +0100

    init version

---
+++ dags/nightly_build.py
@@ -0,0 +1,25 @@
+import time
+from datetime import datetime
+import pendulum
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
+from include.matillion.operators.MatillionTriggerSyncOperator import MatillionTriggerSyncOperator
+
+dag = DAG('DW_nightly_build', description='Matillion nightly execution',
+          schedule_interval=None,
+          start_date=pendulum.datetime(2023, 10, 31, tz="Europe/Oslo"),
+          max_active_runs=1,
+          concurrency=8,
+          catchup=False
+          )
+
+m_Start = EmptyOperator(task_id='Start', dag=dag)
+m_End = EmptyOperator(task_id='End', trigger_rule='all_done', dag=dag)
+
+
+m_Test_Job = MatillionTriggerSyncOperator(task_id="mat_DW_DIMENSIONS", group_name="DW", project_name="DW_Test", job_name="DW_DIMENSIONS", environment_name="Test", dag=dag)
+
+m_Test_Job << [m_Start]
+
+m_End << [m_Test_Job]
